66ec9bacad50cdb9619395c2acb0b48c10e23e3b
Mario Camelo <mariuo@gmail.com>
2021-03-01 13:38:43 -0300

    GenreResource and GenreService Created, and passed all tests from GenreResourceIT

--- backend/src/main/java/com/devsuperior/movieflix/repositories/GenreRepository.java
+++ backend/src/main/java/com/devsuperior/movieflix/repositories/GenreRepository.java
@@ -1,11 +1,17 @@
 package com.devsuperior.movieflix.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.devsuperior.movieflix.entities.Genre;
 
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, Long>{
+	
+	@Query("SELECT obj FROM Genre obj ORDER BY obj.name ASC")
+	List<Genre> findOrderByNameAsc();
 
 }
--- backend/src/main/java/com/devsuperior/movieflix/resources/GenreResource.java
+++ backend/src/main/java/com/devsuperior/movieflix/resources/GenreResource.java
@@ -1,13 +1,11 @@
 package com.devsuperior.movieflix.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.movieflix.dto.GenreDTO;
@@ -20,16 +18,9 @@ public class GenreResource {
 	@Autowired
 	private GenreService service;
 	
-	@GetMapping
-	public ResponseEntity<Page<GenreDTO>> findAll(
-		@RequestParam(value = "page", defaultValue = "0") Integer page,
-		@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-		@RequestParam(value = "direction", defaultValue = "ASC") String direction,
-		@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
-			) {
-		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		
-		Page<GenreDTO> list = service.findAllPaged(pageRequest);
+	@GetMapping	
+	public ResponseEntity<List<GenreDTO>> findAll(){
+		List<GenreDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
