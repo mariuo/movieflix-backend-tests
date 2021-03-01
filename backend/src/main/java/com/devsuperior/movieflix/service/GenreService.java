488da3c27a6a6aa7547c8f04dc626eb2497f6bf4
Mario Camelo <mariuo@gmail.com>
2021-03-01 11:55:53 -0300

    JWT O2Auth created

---
+++ backend/src/main/java/com/devsuperior/movieflix/service/GenreService.java
@@ -0,0 +1,27 @@
+package com.devsuperior.movieflix.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.movieflix.dto.GenreDTO;
+import com.devsuperior.movieflix.entities.Genre;
+import com.devsuperior.movieflix.repositories.GenreRepository;
+
+@Service
+public class GenreService {
+	
+	@Autowired
+	private GenreRepository repository;
+	
+	@Transactional(readOnly = true)
+	public Page<GenreDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Genre> list = repository.findAll(pageRequest);	
+		return list.map(x -> new GenreDTO(x));
+	}
+	
+
+	
+}
