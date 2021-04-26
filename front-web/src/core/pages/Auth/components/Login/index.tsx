2e05e6e3a7047dce80984de1106cb147e594cad3
Mario Camelo <mariuo@gmail.com>
2021-04-26 16:38:02 -0300

    Front: Login Page with Authentication

--- front-web/src/core/pages/Auth/components/Login/index.tsx
+++ front-web/src/core/pages/Auth/components/Login/index.tsx
@@ -1,27 +1,58 @@
-import { Link } from 'react-router-dom';
+
 import ButtonIcon from '../../../../components/ButtonIcon';
 import AuthCard from '../Card';
 import './styles.scss';
+import { useForm } from "react-hook-form";
+import { makeLogin } from 'core/utils/request';
+import { useState } from 'react';
+import { saveSessionData } from 'core/utils/auth';
+import { useHistory } from 'react-router';
+
+type FormData ={
+    username: string;
+    password: string;
+}
 
 const Login = () => {
+    const { register, handleSubmit } = useForm<FormData>();
+    const [hasError, setHasError] = useState(false);
+    const history = useHistory();
+    const onSubmit = (data: FormData) =>{
+        makeLogin(data)
+        .then(response => {
+            setHasError(false);
+            saveSessionData(response.data);
+            history.push('/catalog')
+        })
+        .catch(()=>{
+            setHasError(true);
+        })
+    }
+
     return (
         <AuthCard title="login">
-            <form className="login-form">
+            {hasError && (
+                <div className="alert alert-danger mt-5">
+                    Usuário ou senha inválidos!
+                </div>
+            )}
+            <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
                 <input
                     type="email"
                     className="form-control input-base margin-bottom-30"
-                    placeholder="Email"
+                    placeholder="Email"                    
+                    {...register("username", {required:true})}
+
                 />
                 <input
                     type="password"
                     className="form-control input-base"
-                    placeholder="Senha"
+                    placeholder="Senha"                    
+                    {...register("password", {required:true})}
+                    
                 />
-                <div className="login-submit">
-
-                <Link to="/catalog">
-                    <ButtonIcon text="logar"/>
-                </Link>
+                <div className="login-submit">                 
+                    <ButtonIcon text="logar"/>                
                 </div>
             </form>
         </AuthCard>
---
+++ front-web/src/core/utils/request.ts
@@ -0,0 +1,38 @@
+import axios, {Method} from 'axios';
+import { CLIENT_ID, CLIENT_SECRET } from './auth';
+import qs from 'qs';
+
+type RequestParams = {
+    method?: Method;
+    url?: string;
+    data?: object | string;
+    params?: object;
+    headers?: object;
+}
+
+type LoginData = {
+    username: string;
+    password: string;
+}
+const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+
+export const makeRequest = ({method = 'GET', url, data, params, headers} : RequestParams) => {
+    return axios({
+        method,
+        url: `${BASE_URL}${url}`,
+        data,
+        params,
+        headers
+    });
+};
+export const makeLogin = (LoginData: LoginData) => {
+    const token = `${CLIENT_ID}:${CLIENT_SECRET}`;
+
+    const headers = {
+        Authorization:`Basic ${window.btoa(token)}`,
+        'Content-Type':'application/x-www-form-urlencoded'
+    }
+    const payload = qs.stringify({ ...LoginData, grant_type: 'password'})
+       
+    return makeRequest({url: '/oauth/token', data: payload, method: 'POST', headers})
+}
