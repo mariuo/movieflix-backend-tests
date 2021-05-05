c101736c891f27875703162e0441a1bf5f4620e4
Mario Camelo <mariuo@gmail.com>
2021-05-05 17:13:39 -0300

    Mobile created Routes, and Page-HOME

--- mobile/src/pages/Home.tsx
+++ mobile/src/pages/Home.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Image, View, Text} from 'react-native';
+import {text, theme} from '../styles';
+import homeImg from '../assets/home_draw.png';
+import arrow from '../assets/arrow.png';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
 
 const Home: React.FC = () => {
     return(
-        <View>
-            <Text>
-                Home
-            </Text>
+        <View style={theme.container}>
+            <Image source={homeImg} />
+            <View style={theme.content}>
+                <Text style={text.bold}>Avalie filmes</Text>
+                <Text style={text.regular}>Diga o que você achou do seu filme favorito</Text>
+            </View>
+            <TouchableOpacity style={theme.primaryBtn}>
+                <Text style={text.primaryBtn}> Fazer Login</Text>
+                <View style={theme.arrowContainer}>
+                    <Image source={arrow} />
+                </View>
+            </TouchableOpacity>
         </View>
     );
 };
---
+++ mobile/src/styles/index.ts
@@ -0,0 +1,75 @@
+import { StyleSheet } from 'react-native';
+
+const colors = {
+    primary:"#FFC700",
+    secondary: "#937d1c",
+    black: "#000",
+    darkGray: "#525252",
+    mediumGray: "#6C6C6C",
+    softGray: "#9E9E9E",
+    lightGray: "#CDCDCD",
+    white: "#FFF",
+    red: ""
+}
+
+const text = StyleSheet.create({
+    bold:{
+        fontWeight: "bold",
+        fontSize: 26,
+        color: colors.white,
+        textAlign: 'center',
+    },
+    regular:{
+        fontWeight: "400",
+        fontSize: 16,
+        color: colors.lightGray,
+        marginTop: 50,
+        textAlign: 'center',
+    },
+    primaryBtn:{
+        fontSize: 16,
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        marginLeft: 90,
+    },
+});
+
+const theme = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: "space-around",
+        alignItems: "center",
+        backgroundColor: colors.darkGray,
+    },
+    content:{
+        width: 220,
+        alignItems: "center",
+        justifyContent: "center",
+        textAlign: "center",
+
+    },
+    primaryBtn:{
+        backgroundColor: colors.primary,
+        width: 300,
+        height: 50,
+        borderRadius: 10,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent:"space-between",
+
+    },
+    arrowContainer:{
+        backgroundColor: colors.secondary,
+        width: 50,
+        height: 50,
+        borderTopRightRadius: 10,
+        borderBottomRightRadius: 10,
+        alignItems:"center",
+        justifyContent: "center",
+    }
+
+
+});
+
+
+export {colors, theme, text };
